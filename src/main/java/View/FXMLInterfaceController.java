a7c51bdc948966625188e05a78974ec6c25f92c5
MMs <mugarukamarie@gmail.com>
2016-04-04 15:49:21 +0200

    drawimage:test

--- src/main/java/View/FXMLInterfaceController.java
+++ src/main/java/View/FXMLInterfaceController.java
@@ -5,24 +5,66 @@
  */
 package View;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 /**
- * FXML Controller class
  *
- * @author Anas
+ * @author marie
  */
 public class FXMLInterfaceController implements Initializable {
+    
+    @FXML private Canvas mainCanvas;
+    public GraphicsContext gc;
+    
+    
+    
+    /*public void start() {
+    gc = mainCanvas.getGraphicsContext2D();
+      }*/
 
-    int test = 1;
-    /**
-     * Initializes the controller class.
-     */
+     
+   @FXML
+   
+    public void drawShapes(ActionEvent event)
+    {
+       //gc = mainCanvas.getGraphicsContext2D(); 
+       //gc.setFill(Color.AQUA);
+       //gc.fillRect(10,10,100,100); 
+
+       
+    }
+
+   
+   /* @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }*/
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        gc = mainCanvas.getGraphicsContext2D();
+        Image image4 = new Image("file:C:/Users/marie/Documents/GitHub/BomberMan/src/main/resources/fxml/ECE_COUL_RVB.jpg", 0, 100, false, false);
+        gc.drawImage(image4, 100, 100, 100, 100);    
+       
     }    
     
 }
